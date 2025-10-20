d73cc06a48169085ea92cdc98c6ba9c46a777fe3
maxchadin <maxchadin080418@gmail.com>
2025-10-20 13:01:06 +0300

    main m

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+int ** create(size_t rows, size_t cols);
+void remove(int ** m, size_t rows, size_t cols);
+void input(int ** m, size_t rows, size_t cols);
+void output(const int * const * m, size_t rows, size_t cols);
+
 int main(){
 size_t rows = 0, cols = 0;
 std::cin >> rows >> cols;
 if (!std::cin){
 return 1;}
-std::cout << rows << " " << cols << "\n";
+void input(int ** m, size_t rows, size_t cols){
+for (size_t i==0; i<rous && (std::cin), ++i){
+for (size_t j==0; j<cols && (std::cin),++j){
+std::cin >> m[i][j];
+}
+}
+}
+
+void output(const int * const * m, size_t rows, size_t cols){
+std::cout << rows << " " << cols;
+}
+
+int ** m = create (rows, cols);
+input(m, rows, cols);
+if(!std::cin){
+remove(m, rows, cols);
+return 1;}
+output(m, rows, cols);
+remove(m, rows, cols);
+std::cout << "\n"; 
 }
