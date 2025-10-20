e8e7130ab409d5ba356c565e54262d33f8fa9d59
maxchadin <maxchadin080418@gmail.com>
2025-10-20 13:11:45 +0300

    main m2

--- main.cpp
+++ main.cpp
@@ -4,29 +4,33 @@ void remove(int ** m, size_t rows, size_t cols);
 void input(int ** m, size_t rows, size_t cols);
 void output(const int * const * m, size_t rows, size_t cols);
 
-int main(){
-size_t rows = 0, cols = 0;
-std::cin >> rows >> cols;
-if (!std::cin){
-return 1;}
 void input(int ** m, size_t rows, size_t cols){
-for (size_t i==0; i<rous && (std::cin), ++i){
-for (size_t j==0; j<cols && (std::cin),++j){
-std::cin >> m[i][j];
-}
-}
+	for (size_t i=0; i<rows && (std::cin); ++i){
+		for (size_t j=0; j<cols && (std::cin); ++j){
+			std::cin >> m[i][j];
+                }
+        }
 }
 
 void output(const int * const * m, size_t rows, size_t cols){
-std::cout << rows << " " << cols;
+	std::cout << rows << " " << cols;
 }
 
-int ** m = create (rows, cols);
-input(m, rows, cols);
-if(!std::cin){
-remove(m, rows, cols);
-return 1;}
-output(m, rows, cols);
-remove(m, rows, cols);
-std::cout << "\n"; 
+
+int main(){
+	size_t rows = 0, cols = 0;
+	std::cin >> rows >> cols;
+	if (!std::cin){
+		return 1;
+	}
+
+	int ** m = create (rows, cols);
+	input(m, rows, cols);
+	if(!std::cin){
+		remove(m, rows, cols);
+		return 1;
+	}
+	output(m, rows, cols);
+	remove(m, rows, cols);
+	std::cout << "\n"; 
 }
